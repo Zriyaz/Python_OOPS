3d525d660520062bb247a4e581992e41aff6a413
Md Riyaz Kazmi <mdriyazuddine17@gmail.com>
2020-04-14 04:55:40 +0000

    print(a.Stream)

---
+++ Class_Variable.py
@@ -0,0 +1,19 @@
+class Student:
+  Stream="MCA" #class variable 
+
+  def __init__(self,name,roll): 
+    #instance variables
+    self.name=name
+    self.roll=roll
+  def details(self):
+    print("Name :",self.name)
+    print("Roll No. :",self.roll)
+    print("Branch : ",self.Stream)
+
+  
+a=Student("Riyaz","18mcr014")
+b=Student("Ritesh","18mcr013")
+a.details()
+b.details()
+#direct access class varibale
+print(a.Stream)
